fedf3209b16e6af776f96ea20e759c707ecb76cf
Az <az.dev@e.mail.de>
2024-11-11 19:43:56 +0100

    add checkbox to tasks

---
+++ src/components/Checkbox.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts"></script>
+
+<template>
+	<label class="cursor-pointer">
+		<label class="flex h-5 w-5 items-center justify-center">
+			<input id="checkbox" type="checkbox" />
+			<span></span>
+		</label>
+	</label>
+</template>
+
+<style scoped>
+input[type="checkbox"] {
+	opacity: 0;
+	position: absolute;
+}
+
+input[type="checkbox"] + span {
+	position: relative;
+	width: 20px;
+	height: 20px;
+	border: 2px solid #374151;
+	background: #111827;
+	transition: background-image 0.3s ease;
+}
+
+input[type="checkbox"]:checked + span {
+	background-image: url("assets/check.png");
+	background-size: cover;
+	background-position: center;
+	border-color: #057a55;
+}
+
+input[type="checkbox"]:hover + span {
+	cursor: pointer;
+}
+</style>
--- src/components/tasks/Task.vue
+++ src/components/tasks/Task.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { PropType } from "vue";
 import TaskList from "./TaskList.vue";
+import Checkbox from "../Checkbox.vue";
 
 const props = defineProps({
 	task: {
@@ -12,10 +13,13 @@ const props = defineProps({
 
 <template>
 	<li>
-		<div class="my-3 rounded-lg border-2 border-gray-700 bg-gray-800 p-3">
-			<p>
-				{{ props.task.name }}
-			</p>
+		<div class="my-3 rounded-lg border-2 border-gray-700 bg-gray-800">
+			<div class="flex items-center">
+				<Checkbox class="p-3" />
+				<p>
+					{{ props.task.name }}
+				</p>
+			</div>
 			<p>
 				{{ props.task.description }}
 			</p>
